242a152aea7458666aedaf08b6d6a09283ac0ca1
kruczjak <kruczjak@gmail.com>
2018-12-13 20:42:34 +0100

    Initial commit

---
+++ lib/yabeda/gruf.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'yabeda/gruf/version'
+require 'yabeda'
+
+module Yabeda
+  module Gruf
+    class << self
+      def install!; end
+    end
+  end
+end
---
+++ lib/yabeda/gruf/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Yabeda
+  module Gruf
+    VERSION = '0.1.0'
+  end
+end
